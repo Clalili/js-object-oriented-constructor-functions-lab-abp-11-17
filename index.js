bc96fd4c446bdd7add0bfc1673c4d61838f1faff
Clara <c_c_456@hotmail.com>
2017-11-20 19:25:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-function Scooter(year, color, model){
-  this.year = year,
-  this.color = color,
-  this.model = model
-}
 
-let samson = new scooter(2015, "red", "swoosh");
+let samson = new Scooter(2015, "red", "swoosh");
